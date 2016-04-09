23ac71a78a7c0b17bcf625904ba0ee98a9ab8dec
Noah Swartz <swartzcr@gmail.com>
2016-04-09 10:14:18 -0700

    it works!

---
+++ _dungeon_helper.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python3
+import random
+
+#basics
+wall = u"\u2b1b"
+floor = u"\u2b1c"
+
+#food
+beer = u"\U0001f37a"
+cheese = u"\U0001f9c0"
+chocolate = u"\U0001f36b"
+fruit = u"\U0001f351"
+meat = u"\U0001f356"
+mushroom = u"\U0001f344"
+pizza = u"\U0001f355"
+FOODS = ([beer]*3+
+         [cheese]*5+
+         [chocolate]*2+
+         [fruit]*5+
+         [meat]*10+
+         [mushroom]*2+
+         [pizza]*3)
+
+#items
+bag_of_holding = u"\U0001f45d"
+book = u"\U0001f4d5"
+boots = u"\U0001f462"
+bow_arrow = u"\U0001f3f9"
+brass_torch = u"\U0001f526"
+camera = u"\U0001f4f7"
+candle = u"\U0001f56f"
+crown = u"\U0001f451"
+chest = u"\U0001f381"
+credit_card = u"\U0001f4b3"
+crystal_ball = u"\U0001f52e"
+dagger = u"\U0001f5e1"
+gem = u"\U0001f48e"
+hat = u"\U0001f3a9"
+helmet = u"\u26d1"
+horn = u"\U0001f4ef"
+key = u"\U0001f5dd"
+magic_marker = u"\U0001f58d"
+money_bag = u"\U0001f4b0"
+pack = u"\U0001f392"
+pick_axe = u"\u26cf"
+potion = u"\u2697"
+prayer_beads = u"\U0001f4ff"
+ring = u"\U0001f48d"
+scroll = u"\U0001f4dc"
+shield = u"\U0001f6e1"
+shirt = u"\U0001f455"
+shirt2 = u"\U0001f3bd"
+ITEMS = ([bag_of_holding]*3+
+         [book]*10+
+         [boots]*10+
+         [bow_arrow]*10+
+         [brass_torch]*2+
+         [camera]*2+
+         [candle]*3+
+         [crown]*5+
+         [chest]*5+
+         [credit_card]*3+
+         [crystal_ball]*2+
+         [dagger]*5+
+         [gem]*2+
+         [hat]*3+
+         [helmet]*5+
+         [horn]*2+
+         [key]*3+
+         [magic_marker]*3+
+         [money_bag]*10+
+         [pack]*2+
+         [pick_axe]*5+
+         [potion]*3+
+         [prayer_beads]*2+
+         [ring]*5+
+         [scroll]*10+
+         [shield]*5+
+         [shirt]*2+
+         [shirt2]*3)
+
+#monuments
+bath = u"\U0001f6c1"
+shrine = u"\u26e9"
+statue = u"\U0001f5ff"
+temple = u"\U0001f3db"
+web = u"\U0001f578"
+MONUMENTS = ([bath]*1+
+             [statue]*3+
+             [temple]*2+
+             [shrine]*2+
+             [web]*2)
+
+#doors
+door = u"\U0001f6aa"
+DOORS = [door]
+
+#monsters
+ant = u"\U0001f41c"
+bee = u"\U0001f41d"
+bug = u"\U0001f41b"
+cop = u"\U0001f46e"
+croc = u"\U0001f40a"
+dragon = u"\U0001f409"
+dust_vortex = u"\U0001f32a"
+elephant = u"\U0001f418"
+ghost = u"\U0001f47b"
+imp = u"\U0001f47f"
+monkey = u"\U0001f412"
+ogre = u"\U0001f479"
+ox = u"\U0001f402"
+rat = u"\U0001f400"
+scorpion = u"\U0001f982"
+snake = u"\U0001f40d"
+sheep = u"\U0001f40f"
+unicorn = u"\U0001f984"
+w_buffalo = u"\U0001f403"
+MONSTERS = ([ant]*1+
+            [bee]*1+
+            [bug]*1+
+            [cop]*1+
+            [croc]*5+
+            [dragon]*5+
+            [dust_vortex]*1+
+            [elephant]*5+
+            [ghost]*5+
+            [imp]*3+
+            [monkey]*2+
+            [ogre]*5+
+            [ox]*10+
+            [rat]*1+
+            [scorpion]*5+
+            [snake]*10+
+            [sheep]*5+
+            [unicorn]*5+
+            [w_buffalo]*5)
+#people
+thinking = u"\U0001f914"
+worried = u"\U0001f625"
+dizzy = u"\U0001f635"
+grimace = u"\U0001f62c"
+berserk = u"\U0001f621"
+PLAYERS = [thinking, worried, dizzy, grimace, berserk]
+
+#pets
+egg = u"\U0001f423"
+cat = u"\U0001f408"
+dog = u"\U0001f415"
+horse = u"\U0001f40e"
+PETS = ([egg]*3+
+        [cat]*9+
+        [dog]*7+
+        [horse])
+
+basic_dungeon =     [[wall]*13,
+                    [wall]+[floor]*11 + [wall],
+                    [wall]+[floor]*11 + [wall],
+                    [wall]+[floor]*11 + [wall],
+                    [wall]+[floor]*11 + [wall],
+                    [wall]+[floor]*11 + [wall],
+                    [wall]+[floor]*11 + [wall],
+                    [wall]+[floor]*11 + [wall],
+                    [wall]*13]
+
+DUNGEONS = [basic_dungeon]
+
+MONSTER_CLUSTERS = {bee: 10,
+                    rat: 4,
+                    ox:  4,
+                    w_buffalo: 3,
+                    snake: 5,
+                    bug: 4,
+                    ogre: 6}
+
+path = u"\U0001f3fe"
+
--- dungeon.py
+++ dungeon.py
@@ -1,73 +1,272 @@
-#!/usr/bin/env python3
 import random
+import _dungeon_helper
 
+#DOORS = _dungeon_helper.DOORS
+DUNGEONS = _dungeon_helper.DUNGEONS
+MONSTERS = _dungeon_helper.MONSTERS
+MONSTER_CLUSTERS = _dungeon_helper.MONSTER_CLUSTERS
+ITEMS = _dungeon_helper.ITEMS
+FOODS = _dungeon_helper.FOODS
+MONUMENTS = _dungeon_helper.MONUMENTS
+PLAYERS = _dungeon_helper.PLAYERS
+PETS = _dungeon_helper.PETS
+floor = _dungeon_helper.floor
+wall = _dungeon_helper.wall
 
-dimensions = (14, 10)
-floor = "◽"
-wall = "◼"
-basic_dungeon =     [[wall]*14,
-                    [wall]+[floor]*12 + [wall],
-                    [wall]+[floor]*12 + [wall],
-                    [wall]+[floor]*12 + [wall],
-                    [wall]+[floor]*12 + [wall],
-                    [wall]+[floor]*12 + [wall],
-                    [wall]*14]
+room_tries = 6
+path_tries = 4
+action_tries = 10
+width, height = (13,10)
 
-DUNGEONS = [basic_dungeon]
-MONSTERS = [""]*50+["A"]*30+["B"]*20
-ITEMS = [""]*50+["/"]*20+["$"]*20+["*"]*10
-OBJECTS = [""]*50+["#"]*20+["_"]*30
-THINGS = [MONSTERS, ITEMS, OBJECTS]
-PLAYERS = ["@"]
+def gen_dungeon_border(rooms):
+    out = []
+    for y in range(height):
+        for x in range(width):
+            if y == 0 or y == height-1:
+                out.append((x,y))
+            elif x == 0 or x == width -1:
+                out.append((x,y))
+    out = set(out).difference(rooms)
+    return list(out)
 
-item_tries = 10
-DOORS = ["+"]
+def get_width_height(dungeon):
+    return (len(dungeon[0]), len(dungeon))
+
+def get_random_x_y(dungeon, rooms):
+    return random.choice(list(rooms))
+
+def draw(places, tile, dungeon):
+    for space in places:
+        x, y = space
+        dungeon[y][x] = tile
 
 def gen_dungeon():
-    TEMPLATE_DUNGEON = random.choice(DUNGEONS)
-    dungeon = TEMPLATE_DUNGEON
-    decorate_room(dungeon)
+#    TEMPLATE_DUNGEON = random.choice(DUNGEONS)
+    dungeon, empty_spaces = make_dungeon_outline()
+    rooms, room_list = gen_rooms(empty_spaces)
+    paths = gen_paths(room_list)
+    draw(list(rooms), _dungeon_helper.floor, dungeon)
+    draw(list(paths), _dungeon_helper.path, dungeon)
+    rooms = rooms.union(paths)
+    decorate_room(dungeon, rooms)
+#    dungeon = _dungeon_helper.test_dungeon
     printable_dungeon = format_dungeon(dungeon)
-    #printable_dungeon = dungeon
     return(printable_dungeon)
 
-def decorate_room(dungeon):
-    width, height = (len(dungeon[0]), len(dungeon))
-    add_doors(dungeon, random.randint(0,3))
-    add_player(dungeon)
-    add_things(dungeon, item_tries)
+def gen_rooms(empty_spaces):
+    rooms = set()
+    borders = set()
+    room_list = []
+    for i in range(room_tries):
+        potential_room, border, empty_spaces = make_room(empty_spaces)
+        if rooms.isdisjoint(potential_room) and borders.isdisjoint(potential_room):
+            rooms = rooms.union(potential_room)
+            borders = borders.union(border)
+            room_list.append(list(potential_room))
+    return rooms, room_list
 
-def add_doors(dungeon, num_doors):
-    door = random.choice(DOORS)
-    for i in range(num_doors):
-        placement = random.randint(0,37)
-        if placement < 14:
-            dungeon[0][placement] = door
-        elif placement > 23:
-            dungeon[6][placement-24] = door
+def gen_paths(room_list):
+    paths = set()
+    coords = sorted([[min(room), max(room)] for room in room_list])
+    for idx,room1 in enumerate(coords):
+        for room2 in coords[idx+1:]:
+            x_overlap = sorted(list(set(range(room1[0][0],room1[1][0]+1)).intersection(set(range(room2[0][0], room2[1][0]+1)))))
+            y_overlap = sorted(list(set(range(room1[0][1],room1[1][1]+1)).intersection(set(range(room2[0][1], room2[1][1]+1)))))
+            if len(x_overlap):
+                print("We're going down!!")
+                print((room1, room2))
+                print(range(min(room2[0][1], room1[1][1])+1, max(room1[1][1], room2[0][1])))
+                ys = sorted([room1[0][1], room1[1][1], room2[0][1], room2[1][1]])
+                y_difference = sorted(list(set(range(ys[1]+1, ys[2]))))
+
+                #y_difference = sorted(list(set(range(min([room1[1][1], room2[0][1]])+1,
+                #                                     max([room1[1][1], room2[0][1]])))))
+                print(x_overlap)
+                print(y_difference)
+                path = make_v_path(x_overlap, y_difference)
+                paths = paths.union(path)
+            elif len(y_overlap):
+                print("Everything is going right")
+                print((room1, room2))
+                print(range(min(room2[1][0], room1[1][0])+1, max(room1[1][0], room2[0][0])))
+                xs = sorted([room1[0][0], room1[1][0], room2[0][0], room2[1][0]])
+                x_difference = sorted(list(set(range(xs[1]+1, xs[2]))))
+                #x_difference = sorted(list(set(range(min([room1[1][0], room2[0][0]])+1,
+                #                                     max([room1[1][0], room2[0][0]])))))
+                print(y_overlap)
+                print(x_difference)
+                path = make_h_path(x_difference, y_overlap)
+                paths = paths.union(path)
+                continue
+    return paths
+
+def make_v_path(x, y):
+    path = set()
+    if len(y) == 1:
+        path.add((random.choice(x), y[0]))
+        return path
+    x1 = random.choice(x)
+    y1 = y[0]
+    print((x1,y1))
+    path.add((x1, y1))
+    y1 += 1
+    print((x1,y1))
+    path.add((x1, y1))
+    xendpoint = random.choice([x[0], x[-1]])
+    if xendpoint > x1:
+        direction = 1
+    else:
+        direction = -1
+    while y1 != y[-1]:
+        if x1 != xendpoint and random.choice([0,1]):
+            x1 += direction
+            print((x1,y1))
+            path.add((x1, y1))
         else:
-            dungeon[int((placement-14)/2)][-(placement%2)] = door #ewww
+            y1 += 1
+            print((x1,y1))
+            path.add((x1, y1))
+    return path
 
-def add_player(dungeon):
-    width, height = (len(dungeon[0]), len(dungeon))
+def make_h_path(x, y):
+    path = set()
+    if len(x) == 1:
+        path.add((x[0], random.choice(y)))
+        return path
+    x1 = x[0]
+    y1 = random.choice(y)
+    print((x1,y1))
+    path.add((x1, y1))
+    x1 += 1
+    print((x1,y1))
+    path.add((x1,y1))
+    yendpoint = random.choice([y[0], y[-1]])
+    if yendpoint > y1:
+        direction = 1
+    else:
+        direction = -1
+    while x1 != x[-1]:
+        if y1 != yendpoint and random.choice([0,1]):
+            y1 += direction
+            print((x1,y1))
+            path.add((x1, y1))
+        else:
+            x1 += 1
+            print((x1,y1))
+            path.add((x1, y1))
+    return path
+    return path
+
+def make_dungeon_outline():
+    empty_spaces = set()
+    for i in range(10):
+        for j in range(13):
+            empty_spaces.add((j,i))
+    return [[_dungeon_helper.wall]*13 for i in range(10)], empty_spaces
+
+def make_room(empty_spaces):
+    startx = random.randint(0,11)
+    starty = random.randint(0,8)
+    x = random.randint(2,8)
+    y = random.randint(2,8)
+    room = set()
+    border = set()
+    for i in range(y):
+        for j in range(x):
+            if i == 0:
+                if 0 < (starty-1):
+                    border.add((min([startx+j, width-1]), starty-1))
+            elif i == y-1:
+                if i+starty < height:
+                    border.add((min([startx+j, width-1]), starty+i+1))
+            if j == 0:
+                if 0 < (startx-1):
+                    border.add((startx-1, min([starty+i, height-1])))
+            elif j == x-1:
+                if j+startx < width:
+                    border.add((startx+j+1, min([starty+i, height-1])))
+            room.add((min([startx+j, width-1]),min([starty+i,height-1])))
+    empty_spaces = empty_spaces.difference(room)
+    return room, border, empty_spaces
+
+def decorate_room(dungeon, rooms):
+    x, y = add_player(dungeon, rooms)
+    for i in range(action_tries):
+        action = ACTIONS.pop(random.randint(0, len(ACTIONS)-1))
+        action(dungeon, rooms)
+
+def add_player(dungeon, rooms):
     player = random.choice(PLAYERS)
-    x = random.randint(1, width-2)
-    y = random.randint(1, height-2)
+    x, y = get_random_x_y(dungeon, rooms)
     dungeon[y][x] = player
+    return (x, y)
 
-def add_things(dungeon, item_tries):
-    width, height = (len(dungeon[0]), len(dungeon))
-    for i in range(item_tries):
-        x = random.randint(1, width-2)
-        y = random.randint(1, height-2)
-        if dungeon[y][x] == floor:
-            thing = random.choice(random.choice(THINGS))
-            if thing:
-                dungeon[y][x] = thing
+def add_pet(dungeon, x, y):
+    x = x + random.randint(-1,1)
+    y = y + random.randint(-1,1)
+    if dungeon[y][x] == floor:
+        dungeon[y][x] = random.choice(PETS)
+
+#def add_door(dungeon):
+#    door = random.choice(DOORS)
+#    width, height = get_width_height(dungeon)
+#    pos_doors = (width*2) + ((height-2)*2)
+#    placement = random.randint(0, pos_doors)
+#    if placement < width:
+#        dungeon[0][placement] = door
+#    elif placement > pos_doors-width:
+#        dungeon[-1][placement-(pos_doors - width)] = door
+#    else:
+#        dungeon[int((placement-13)/2)][-(placement%2)] = door #ewww
+
+def place_monster(dungeon, rooms):
+    x, y = get_random_x_y(dungeon, rooms)
+    if dungeon[y][x] == floor:
+        monster = random.choice(MONSTERS)
+        if monster:
+            dungeon[y][x] = monster
+
+def place_monster_cluster(dungeon, rooms):
+    width, height = get_width_height(dungeon)
+    x, y = get_random_x_y(dungeon, rooms)
+    monster = random.choice(list(MONSTER_CLUSTERS.keys()))
+    for i in range(MONSTER_CLUSTERS[monster]):
+        if 0 < x < width and 0 < y < height and dungeon[y][x] == floor:
+            dungeon[y][x] = monster
+            y += random.randint(-1,1)
+            x += random.randint(-1,1)
+
+
+def place_food(dungeon, rooms):
+    x, y = get_random_x_y(dungeon, rooms)
+    if dungeon[y][x] == floor:
+        food = random.choice(FOODS)
+        if food:
+            dungeon[y][x] = food
+
+def place_item(dungeon, rooms):
+    x, y = get_random_x_y(dungeon, rooms)
+    if dungeon[y][x] == floor:
+        item = random.choice(ITEMS)
+        if item:
+            dungeon[y][x] = item
+
+def place_monument(dungeon, rooms):
+    x, y = get_random_x_y(dungeon, rooms)
+    if dungeon[y][x] == floor:
+        monument = random.choice(MONUMENTS)
+        if monument:
+            dungeon[y][x] = monument
 
 def format_dungeon(dungeon):
     return "\n".join(["".join(row) for row in dungeon])
 
-def random_obj():
-    rand_type = types[random.randint(0, len(types)-1)]
-    return rand_type[random.randint(0, 99)]
+def do_nothing(a, b):
+    return
+
+ACTIONS = ([place_monster]*10+
+           [place_monster_cluster]*2+
+           [place_item]*5+
+           [place_food]*3+
+           [place_monument]*2+
+           [do_nothing]*8)
